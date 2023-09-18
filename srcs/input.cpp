3ca36d69f1925b2031d2de95c3aa67077d91c75f
averdon <anthonyverdon10@gmail.com>
2023-09-18 08:45:07 +0200

    add a button to change the display mode (F1)

--- srcs/input.cpp
+++ srcs/input.cpp
@@ -1,7 +1,26 @@
 #include "main.hpp"
 
+void changeDisplayMode(unsigned int action)
+{
+    static bool wireFrameMode = false;
+    static bool keyEnable = true;
+
+    if (action == GLFW_PRESS && keyEnable == true)
+    {
+        if (wireFrameMode == false)
+            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        else
+            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+        wireFrameMode = !wireFrameMode;
+        keyEnable = false;
+    }
+    else if (action == GLFW_RELEASE)
+        keyEnable = true;
+}
+
 void processInput(GLFWwindow *window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+    changeDisplayMode(glfwGetKey(window, GLFW_KEY_F1));
 }
