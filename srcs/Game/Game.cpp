2806da5ad2ce07342fb4ff8e204d88adf8e4e13a
averdon <anthonyverdon10@gmail.com>
2025-03-20 10:04:56 +0100

    erase .obj handling, only keep it in obj branch

--- srcs/Game/Game.cpp
+++ srcs/Game/Game.cpp
@@ -1,5 +1,4 @@
 #include "Game/Game.hpp"
-#include "ObjectClasses/ObjectParser/ObjectParser.hpp"
 #include "WindowManager/WindowManager.hpp"
 #include "RessourceManager/RessourceManager.hpp"
 #include "ModelLoader/ModelLoader.hpp"
@@ -17,22 +16,11 @@ void scroll_callback(GLFWwindow *window, double xOffset, double yOffset);
 Game::Game() 
 {
     camera = Camera();
-    displayColor = true;
-    mixedValue = 0;
-    for (size_t i = 0; i < 3; i++)
-    {
-        inputRotation[i] = 0;
-        sceneRotation[i] = 0;
-    }
-    axis[X_AXIS] = {1, 0, 0};
-    axis[Y_AXIS] = {0, 1, 0};
-    axis[Z_AXIS] = {0, 0, 1};
 
     WindowManager::SetUserPointer(&camera);
     WindowManager::SetScrollCallback(scroll_callback);
     WindowManager::SetInputMode(GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
-    RessourceManager::AddShader("basic_shader", "shaders/shader.vs", "shaders/shader.fs");
     RessourceManager::AddShader("mesh_shader", "shaders/meshShader.vs", "shaders/meshShader.fs");
     RessourceManager::AddShader("light", "shaders/lightShader.vs", "shaders/lightShader.fs");
 
@@ -55,22 +43,14 @@ void Game::LoadObjects(int argc, char **argv)
 {
     for (int i = 1; i < argc; i++)
     {
-        if (Toolbox::checkExtension(argv[i], ".obj"))
-        {
-            std::vector<Object> newObjects = ObjectParser().parseObjectFile(argv[i]);
-            objects.insert(objects.end(), newObjects.begin(), newObjects.end());
-        }
-        else if (Toolbox::checkExtension(argv[i], ".glb"))
+        if (Toolbox::checkExtension(argv[i], ".glb"))
         {
             std::vector<Model> newModels =  ModelLoader::LoadModel(argv[i]);
             models.insert(models.end(), newModels.begin(), newModels.end());
         }
     }
-    for (size_t i = 0; i < objects.size(); i++)
-        objects[i].initVAO();
     for (size_t i = 0; i < models.size(); i++)
         models[i].Init();
-
 }
 
 void Game::Run()
@@ -86,7 +66,8 @@ void Game::ProcessInput()
     bool focusRenderer = WindowManager::GetInputMode(GLFW_CURSOR) == GLFW_CURSOR_DISABLED;
     if (!HoverOrFocusImGUI && WindowManager::IsMouseButtonPressed(GLFW_MOUSE_BUTTON_1))
         WindowManager::SetInputMode(GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-    static bool keyEnable = true;
+    
+    static bool keyEnable = true; //@todo when creating engine submodule, replace that
     if (keyEnable && WindowManager::IsKeyPressed(GLFW_KEY_ESCAPE))
     {
         if (focusRenderer)
@@ -100,11 +81,7 @@ void Game::ProcessInput()
         keyEnable = true;
     }
     if (focusRenderer)
-    {
         updateCamera();
-        updateSceneOrientation();
-        updateDisplayMode();
-    }
 }
 
 
@@ -141,180 +118,30 @@ void Game::updateCamera()
         camera.setPitch(89.0f);
     else if (camera.getPitch() < -89.0f)
         camera.setPitch(-89.0f);
-}
 
-void Game::updateSceneOrientation()
-{
-    inputRotation[X_AXIS] = WindowManager::IsKeyPressed(GLFW_KEY_U) - WindowManager::IsKeyPressed(GLFW_KEY_Y);
-    inputRotation[Y_AXIS] = WindowManager::IsKeyPressed(GLFW_KEY_J) - WindowManager::IsKeyPressed(GLFW_KEY_H);
-    inputRotation[Z_AXIS] = WindowManager::IsKeyPressed(GLFW_KEY_M) - WindowManager::IsKeyPressed(GLFW_KEY_N);
-}
-
-void Game::updateDisplayMode()
-{
-    updateWireframeMode();
-    updateTextureMode();
-}
-
-void Game::updateWireframeMode()
-{
-    static bool wireFrameMode = false;
-    static bool keyEnable = true;
-
-    if (WindowManager::IsKeyPressed(GLFW_KEY_F1))
-    {
-        if (keyEnable == true)
-        {
-            wireFrameMode = !wireFrameMode;
-            if (wireFrameMode)
-                glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-            else
-                glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-        }
-        keyEnable = false;
-    }
-    else
-        keyEnable = true;
-}
-
-void Game::updateTextureMode()
-{
-    static bool keyEnable = true;
-
-    if (WindowManager::IsKeyPressed(GLFW_KEY_F2))
-    {
-        if (keyEnable == true)
-            displayColor = !displayColor;
-        keyEnable = false;
-    }
-    else
-        keyEnable = true;
-}
-
-void Game::updateScene()
-{
-    updateCameraView();
-    updateTexture();
-    
-    for (size_t i = 0; i < objects.size(); i++)
-        renderObject(objects[i]);
-    
-    {
-        sceneRotation[X_AXIS] += inputRotation[X_AXIS] * Time::getDeltaTime();
-        sceneRotation[Y_AXIS] += inputRotation[Y_AXIS] * Time::getDeltaTime();
-        sceneRotation[Z_AXIS] += inputRotation[Z_AXIS] * Time::getDeltaTime();
-        ml::mat4 rotation;
-        rotation.identity();
-        rotation =  ml::rotate(sceneRotation[X_AXIS], axis[X_AXIS]) *
-        ml::rotate(sceneRotation[Y_AXIS], axis[Y_AXIS]) *
-        ml::rotate(sceneRotation[Z_AXIS], axis[Z_AXIS]);
-        
-        ml::mat4 projection = ml::perspective(camera.getFov(), (float)WINDOW_WIDTH / (float)WINDOW_HEIGHT, 0.1f, 100.0f);
-        
-        ml::mat4 view = ml::lookAt(camera.getPosition(), camera.getPosition() + camera.getFrontDirection(),
-        camera.getUpDirection());
-        for (size_t i = 0; i < models.size(); i++)
-            models[i].Draw(camera.getPosition(), light, projection, view);
-    }
-    
-    // light
-    auto shader = RessourceManager::GetShader("light");
-    shader->use();
-    
-    ml::mat4 projection = ml::perspective(camera.getFov(), (float)WINDOW_WIDTH / (float)WINDOW_HEIGHT, 0.1f, 100.0f);
-    shader->setMat4("projection", projection);
-    
-    ml::mat4 view = ml::lookAt(camera.getPosition(), camera.getPosition() + camera.getFrontDirection(), camera.getUpDirection());
-    shader->setMat4("view", view);
-    
-    ml::mat4 model = ml::translate(light.GetPos()) * ml::rotate(45, ml::vec3(1, 1, 1)) * ml::scale(light.GetScale());
-    shader->setMat4("model", model);
-    
-    shader->setVec3("lightColor", light.GetColor());
-
-    light.Draw();
-}
-
-void Game::updateCameraView()
-{
     ml::vec3 direction(cosf(Toolbox::DegToRad(camera.getYaw())) * cosf(Toolbox::DegToRad(camera.getPitch())),
-                    sinf(Toolbox::DegToRad(camera.getPitch())),
-                    sinf(Toolbox::DegToRad(camera.getYaw())) * cosf(Toolbox::DegToRad(camera.getPitch())));
+                sinf(Toolbox::DegToRad(camera.getPitch())),
+                sinf(Toolbox::DegToRad(camera.getYaw())) * cosf(Toolbox::DegToRad(camera.getPitch())));
     camera.setFrontDirection(ml::normalize(direction));
     camera.setRightDirection(ml::normalize(ml::crossProduct(camera.getFrontDirection(), camera.getUpDirection())));
 }
 
-void Game::updateTexture()
+void Game::updateScene()
 {
-    if (displayColor == true && mixedValue > 0)
-    {
-        mixedValue -= Time::getDeltaTime();
-        if (mixedValue < 0)
-            mixedValue = 0;
-    }
-    else if (displayColor == false && mixedValue < 1)
-    {
-        mixedValue += Time::getDeltaTime();
-        if (mixedValue > 1)
-            mixedValue = 1;
-    }
-}
-
-void Game::renderObject(const Object &object)
-{
-    updateShader(object);
-    glBindVertexArray(object.getVAO());
-    glDrawElements(GL_TRIANGLES, object.getFaces().size() * 3, GL_UNSIGNED_INT, 0);
-}
-
-void Game::updateShader(const Object &object)
-{
-    auto shader = RessourceManager::GetShader("basic_shader");
-    auto texture = RessourceManager::GetTexture("my_little_pony");
-    shader->use();
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, texture->getID());
-    shader->setInt("texture1", 0);
-    shader->setFloat("aMixValue", mixedValue);
-
-    sceneRotation[X_AXIS] += inputRotation[X_AXIS] * Time::getDeltaTime();
-    sceneRotation[Y_AXIS] += inputRotation[Y_AXIS] * Time::getDeltaTime();
-    sceneRotation[Z_AXIS] += inputRotation[Z_AXIS] * Time::getDeltaTime();
-    ml::mat4 rotation;
-    rotation.identity();
-    rotation = ml::rotate(sceneRotation[X_AXIS], axis[X_AXIS]) *
-                ml::rotate(sceneRotation[Y_AXIS], axis[Y_AXIS]) *
-                ml::rotate(sceneRotation[Z_AXIS], axis[Z_AXIS]);
-    shader->setMat4("rotation", rotation);
-
     ml::mat4 projection = ml::perspective(camera.getFov(), (float)WINDOW_WIDTH / (float)WINDOW_HEIGHT, 0.1f, 100.0f);
-    shader->setMat4("projection", projection);
-
-    ml::mat4 view = ml::lookAt(camera.getPosition(), camera.getPosition() + camera.getFrontDirection(),
-                                 camera.getUpDirection());
-    shader->setMat4("view", view);
-    ml::mat4 model;
-    model.identity();
-    shader->setMat4("model", model);
-
-    shader->setVec3("viewPos", camera.getPosition());
-
-    std::vector<Material> materials = object.getMaterials();
-    for (size_t i = 0; i < materials.size(); i++)
-    {
-        ml::vec3 color = {materials[i].getColor(AMBIANT_COLOR)[0], materials[i].getColor(AMBIANT_COLOR)[1], materials[i].getColor(AMBIANT_COLOR)[2]};
-        shader->setVec3("materials[" + std::to_string(i) + "].ambient", color);
-        color = {materials[i].getColor(DIFFUSE_COLOR)[0], materials[i].getColor(DIFFUSE_COLOR)[1], materials[i].getColor(DIFFUSE_COLOR)[2]};
-        shader->setVec3("materials[" + std::to_string(i) + "].diffuse", color);
-        color = {materials[i].getColor(SPECULAR_COLOR)[0], materials[i].getColor(SPECULAR_COLOR)[1], materials[i].getColor(SPECULAR_COLOR)[2]};
-        shader->setVec3("materials[" + std::to_string(i) + "].specular", color);
-        shader->setFloat("materials[" + std::to_string(i) + "].shininess", 32.0f);
-    }
+    ml::mat4 view = ml::lookAt(camera.getPosition(), camera.getPosition() + camera.getFrontDirection(), camera.getUpDirection());
     
-    shader->setVec3("light.position", light.GetPos()); 
-    shader->setVec3("light.ambient",  light.GetColor());
-    shader->setVec3("light.diffuse",  0.5f, 0.5f, 0.5f);
-    shader->setVec3("light.specular", 1.0f, 1.0f, 1.0f); 
+    // models
+    for (size_t i = 0; i < models.size(); i++)
+        models[i].Draw(camera.getPosition(), light, projection, view);
+    
+    // light
+    auto shader = RessourceManager::GetShader("light");
+    shader->use();
+    ml::mat4 model = ml::translate(light.GetPos()) * ml::rotate(45, ml::vec3(1, 1, 1)) * ml::scale(light.GetScale());
+    shader->setMat4("model", model);
+    shader->setVec3("lightColor", light.GetColor());
+    light.Draw();
 }
 
 void Game::DrawImGui()
--- srcs/Game/Game.hpp
+++ srcs/Game/Game.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "AProgram/AProgram.hpp"
-#include "ObjectClasses/Object/Object.hpp"
 #include "Model/Model.hpp"
 #include "Camera/Camera.hpp"
 #include "Texture/Texture.hpp"
@@ -19,28 +18,14 @@ class Game: public AProgram
 {
     private:
         Camera camera;
-        std::vector<Object> objects;
         std::vector<Model> models;
-        bool displayColor;
         bool HoverOrFocusImGUI;
-        float mixedValue;
-        float sceneRotation[3];
-        float inputRotation[3];
-        ml::vec3 axis[3];
         Light light;
         
         void DrawImGui();
         void ProcessInput();
         void updateCamera();
-        void updateSceneOrientation();
-        void updateDisplayMode();
-        void updateWireframeMode();
-        void updateTextureMode();
         void updateScene();
-        void updateCameraView();
-        void updateTexture();
-        void renderObject(const Object &object);
-        void updateShader(const Object &object);
     
     public:
         Game();
